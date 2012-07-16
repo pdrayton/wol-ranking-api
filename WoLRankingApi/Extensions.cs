bc950811fd83d6e64eb05e8f2b898625e7e7f3e1
Peter Drayton <pdrayton@gmail.com>
2012-07-15 17:15:15 -0700

    Multiple endpoints to test out async/Task support

---
+++ WoLRankingApi/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WoLRankingApi {
+    public static class Extensions {
+        public static IEnumerable<T> AsNotNull<T>(this IEnumerable<T> original) {
+            return original ?? new T[0];
+        } 
+    }
+}
